efa926b5496d094885f896223b7ee6ac54d4c5eb
Josiel <cjosiel2@gmail.com>
2025-02-06 17:02:26 -0300

    Implementado classe Middleware

---
+++ MeuMiddleware.cs
@@ -0,0 +1,18 @@
+﻿namespace DemoVS
+{
+    public class MeuMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public MeuMiddleware(RequestDelegate next) 
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            //httpContext.Response.Headers.Add("X-Meu-Middleware", "Funcinou");
+            await _next(httpContext);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,19 +1,28 @@
 //Gerando Builder
 using CadastroClienteRepos;
+using DemoVS;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 
-RepositorioClientes repositorioClientes = new RepositorioClientes();
+//RepositorioClientes repositorioClientes = new RepositorioClientes();
+//repositorioClientes.CadastrarNome();
+//string msg = repositorioClientes.ImprimirClientes();
 
+app.UseMiddleware<MeuMiddleware>();
 
-repositorioClientes.CadastrarNome();
-string msg = repositorioClientes.ImprimirClientes();
-
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapGet("/", async context =>
+    {
+        await context.Response.WriteAsync("Olá, mundo!");
+    });
+});
 
 //Configurações de comportamento
-app.MapGet("/", () => msg);
+//app.MapGet("/", () => msg);
 
 
 app.Run();
